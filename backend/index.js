e71279d148ce096e7824f1a4a74ee3fb6dab978a
Felipe <felipeldorea@gmail.com>
2022-05-30 22:22:59 -0300

    Setting up socket.io and router

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,20 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router);
+
+io.on('connection', (socket) => {
+	console.log('A user has connected!');
+
+	socket.on('disconnect', () => {
+		console.log('A user has disconnected!');
+	});
+});
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
