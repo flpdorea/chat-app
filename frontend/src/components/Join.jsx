5871df7e68771832a6304b529032b5efb45e7d12
Felipe <felipeldorea@gmail.com>
2022-05-30 23:30:22 -0300

    Implementing name and room selection

--- frontend/src/components/Join.jsx
+++ frontend/src/components/Join.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
-	return <h1>Join</h1>;
+	const [name, setName] = useState('');
+	const [room, setRoom] = useState('');
+
+	return (
+		<div className='joinOuterCointainer'>
+			<div className='joinInnerCointainer'>
+				<h1 className='heading'>Join</h1>
+				<div><input placeholder='Name' className='joinInput' type='text' onChange={(event) => { setName(event.target.value) }} /></div>
+				<div><input placeholder='Room' className='joinInput' type='text' onChange={(event) => { setRoom(event.target.value) }} /></div>
+				<Link onClick={event => (!name || !room ? event.preventDefault() : null)} to={`/chat?name=${name}&room=${room}`}>
+					<button className='button' type='submit'>Sign In</button>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Join;
